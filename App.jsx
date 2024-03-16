75a6ba80412555bb85efa9e5aaf98c006f0ab40c
FrankBosse <francis.bosse@edu.sait.ca>
2024-03-15 22:01:56 -0600

    Lab 4 Submission
    I also added the first optional code of Clear the task input after it's been added

--- App.jsx
+++ App.jsx
@@ -4,23 +4,22 @@
  * @format
  */
 
-import React, { useState } from 'react';
-import { SafeAreaView } from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm'; // Import ToDoForm component
 
 function App() {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
 
-  const [tasks, setTasks] = useState([
-      'Do laundry',
-      'Go to gym',
-      'Walk dog'
-  ]);
+  const addTask = taskText => {
+    setTasks([...tasks, taskText]);
+  };
 
   return (
     <SafeAreaView>
       <ToDoList tasks={tasks} />
-      <ToDoForm />
+      <ToDoForm addTask={addTask} />
     </SafeAreaView>
   );
 }
--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -5,19 +5,30 @@
  */
 
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import {View, TextInput, Button} from 'react-native';
+import {useState} from 'react';
 
 // Import the styles from AppStyles.jsx
 import styles from './AppStyles';
 
-function ToDoForm() {
+function ToDoForm({addTask}) {
+  const [taskText, setTaskText] = React.useState('');
+
   return (
     <View style={styles.form}>
       <TextInput
         style={styles.input}
         placeholder="Add a new task..."
+        onChangeText={text => setTaskText(text)}
+        value={taskText}
       />
-      <Button title="Add" />
+      <Button
+        title="Add Task"
+        onPress={() => {
+          setTaskText('');
+          addTask(taskText);
+        }}
+      />
     </View>
   );
 }

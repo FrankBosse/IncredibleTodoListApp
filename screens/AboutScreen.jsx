24d884fc94324f29128eda5b7a50214688aa34fc
FrankBosse <francis.bosse@edu.sait.ca>
2024-04-05 23:19:20 -0600

    updated AboutScreen
    added navigation back to home

--- screens/AboutScreen.jsx
+++ screens/AboutScreen.jsx
@@ -7,8 +7,9 @@
 import React from 'react';
 import {Text} from 'react-native';
 import MainLayout from '../layouts/MainLayout';
+import {Button} from 'react-native';
 
-function AboutScreen() {
+function AboutScreen({navigation}) {
   const appName = 'My To Do List App';
   const myName = 'Francis Bosse';
   const currentDate = new Date().toLocaleDateString();
@@ -18,6 +19,7 @@ function AboutScreen() {
       <Text>App Name: {appName}</Text>
       <Text>Developer: {myName}</Text>
       <Text>Date: {currentDate}</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </MainLayout>
   );
 }
